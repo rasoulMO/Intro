80d0e8c6659dd1f933ff58b2e88bc3e9f89e81cb
rasoul98 <rasoolmhhmed@gmail.com>
2020-12-30 07:34:15 +0100

    first commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import {Card } from '../card/card.component'
+
+import './card-list.style.css';
+
+
+
+export const CardList = props => {
+  return <div className='card-list'>
+     {props.monsters.map(monster => (
+          <Card key={monster.id} monster={monster} /> 
+    ))}  
+  </div>; 
+};
